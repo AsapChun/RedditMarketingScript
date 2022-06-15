41af09167ae11dd4ca505b2fec6f3e70e98806bd
Sean Chun <schun@bu.edu>
2022-06-15 00:04:40 -0700

    rate limiting

--- reddit.py
+++ reddit.py
@@ -18,7 +18,7 @@ reddit = praw.Reddit(client_id =REDDIT_CLIENT_ID,
                      password =REDDIT_PASSWORD)
 
 #enter subreddits we want to message to
-list_of_subreddits = set(['financialindependence'])
+list_of_subreddits = set(['digitalnomad'])
 user_subreddit_dic = {}
 
 #Grab and send messages to post author and subsequent comments in searched subreddits
@@ -43,8 +43,8 @@ for subreddit in list_of_subreddits:
 
 # TODO: Rate limit handling--> add timer to pause sending messages
 def rateLimitHandling():
-  print("Pausing for 5 mins")
-  time.sleep(60)
+  # print("Pausing for 5 mins")
+  # time.sleep(60)
   print("Pausing for 4 mins")
   time.sleep(60)
   print("Pausing for 3 mins")
@@ -61,21 +61,25 @@ for subreddit in list_of_subreddits:
   user_list = user_subreddit_dic[subreddit]
   for user in user_list:
     try:
-      if (numOfMessages >= 10) :
-        rateLimitHandling()
-        numOfMessages = 0
-      
-      message= (f"Hey, {user} hope you are well! I’m a recent college grad / aspiring entrepreneur on r/{subreddit}." 
-      " I'm building a budgeting chrome extension (similar to honey, except we show you your personal finances instead"  
-      " of coupons, so you can make informed shopping decisions). I made it because I have a ton of anxiety buying stuff lmao," 
-      " I’m still not used to having a salary compared to being broke in college. "  
-      " We have a user testing experiment out right now; do you mind checking it out and lmk what you think of our idea?" 
-      f" We are actively building it rn and would love feedback. You can find it here: https://t.maze.co/92145647?reddituser={user}&subreddit={subreddit} "
-      " I also recognize that I could seem scammy sending you a link; Maze is a trusted industry survey tool used by huge companies like Uber."
-      " Our survey is vetted and hosted entirely by them, you can see their website here: https://maze.co/ " 
-      " Thanks again!")
-      title = "Aspiring entreprenuer looking to get your feedback!"
+      if (numOfMessages == 50) :
+        break
+
+      message= (f"Hey {user} hope you are well! I'm a digital nomad / aspiring entrepreneur,"
+      f" it's great to meet you! I saw you were in r/{subreddit} and thought this may be applicable to you; "
+      "would love to tell you more about my idea, but want to make sure you're interested first to avoid spamming you")
+
+      # message= (f"Hey, {user} hope you are well! I’m a recent college grad / aspiring entrepreneur on r/{subreddit}." 
+      # " I'm building a budgeting chrome extension (similar to honey, except we show you your personal finances instead"  
+      # " of coupons, so you can make informed shopping decisions). I made it because I have a ton of anxiety buying stuff lmao," 
+      # " I’m still not used to having a salary compared to being broke in college. "  
+      # " We have a user testing experiment out right now; do you mind checking it out and lmk what you think of our idea?" 
+      # f" We are actively building it rn and would love feedback. You can find it here: https://t.maze.co/92145647?reddituser={user}&subreddit={subreddit} "
+      # " I also recognize that I could seem scammy sending you a link; Maze is a trusted industry survey tool used by huge companies like Uber."
+      # " Our survey is vetted and hosted entirely by them, you can see their website here: https://maze.co/ " 
+      # " Thanks again!")
+      title = "A potential solution for budget traveling!"
       reddit.redditor(user).message(subject= title, message= message)
+      rateLimitHandling()
       print("message successfully sent to " + user)
       numOfMessages += 1
     except Exception as e:
